1e7cc924d2e44c0c14f4987b351c610bfd299765
omartabbaa <omartabbaa0@gmail.com>
2024-06-17 22:47:40 +0200

    Initial commit

--- src/ElectricAttack.java
+++ src/ElectricAttack.java
@@ -1,20 +1,2 @@
-public interface ElectricAttack {
-    @Override
-    ElectricAttack(String name, int level, int hp, String food, String sound);
-    public void thunderPunch(Pokemon attacker, Pokemon enemy);
-    public void ElectricPokemon(String name, int level, int hp, String food, String sound);
-
-    @Override
-    ElectricAttack(String name, int level, int hp, String food, String sound);
-
-    @Override
-    ElectricAttack(String name, int level, int hp, String food, String sound);
-
-    void ThunderPunch(Pokemon attacker, Pokemon enemy);
-
-    public void ElectroBall(Pokemon attacker, Pokemon enemy);
-    public void Thunder(Pokemon attacker, Pokemon enemy);
-    public void VoltTackle(Pokemon attacker, Pokemon enemy);
-
-
+package PACKAGE_NAME;public interface ElectricAttack {
 }
